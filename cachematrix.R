7b3bed7dd82493ec0d93cfd7b3e70583f7d5ddac
arpita1226 <arpita1226@users.noreply.github.com>
2014-06-19 20:50:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating a special vector to get and set the solve value 
+## for matrix inverse
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
 
+m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
 
 ## Write a short comment describing this function
 
